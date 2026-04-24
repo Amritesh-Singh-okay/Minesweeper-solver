9b61b5c4c7cf25322fe2760533d8fd13c504fdf2
Impossible Golem <amriteshsingh132@gmail.com>
2026-04-24 15:04:47 +0530

    single cell detection with cv2.rectangle

--- Capture.py
+++ Capture.py
@@ -1,9 +1,12 @@
 import pyautogui
 import cv2 as cv
 
-img1 = pyautogui.screenshot('game.png',region=[578,212,748,699])
-print(img1)
+# 578,212,748,699 for complete board
+
+board = pyautogui.screenshot('game.png',region=[578,287,748,624])
+print(board)
 
 img = cv.imread("D:/code/Python/Minesweeper/game.png")
+show = cv.rectangle(img,(0,0),(31,31),(0, 0, 0),1)
 cv.imshow("Display window", img)
 k = cv.waitKey(0)
