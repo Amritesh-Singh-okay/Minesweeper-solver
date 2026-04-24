d536ba8f4890f8071797569adc2deeb2fe77fb0f
Impossible Golem <amriteshsingh132@gmail.com>
2026-04-24 15:40:52 +0530

    full grid drawn over board with cv2.rectangle

--- Capture.py
+++ Capture.py
@@ -4,9 +4,14 @@ import cv2 as cv
 # 578,212,748,699 for complete board
 
 board = pyautogui.screenshot('game.png',region=[578,287,748,624])
-print(board)
+# print(board)
 
 img = cv.imread("D:/code/Python/Minesweeper/game.png")
-show = cv.rectangle(img,(0,0),(31,31),(0, 0, 0),1)
+
+# there is a small error because i cant use decimal val in there it should be 31.34 smt
+for x in range(24):
+    for y in range(20):
+        cv.rectangle(img,(x*31,y*31),((x*31)+31,(y*31)+31),(0, 0, 0),2)
+
 cv.imshow("Display window", img)
 k = cv.waitKey(0)
