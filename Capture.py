6e0106254602b83a3115eb9ce3f0d4a9119d3cbe
Impossible Golem <amriteshsingh132@gmail.com>
2026-04-24 14:28:58 +0530

    displayed minesweepers board using OpenCV

--- Capture.py
+++ Capture.py
@@ -1,4 +1,9 @@
 import pyautogui
+import cv2 as cv
 
-img1 = pyautogui.screenshot('game.png',region=[578,212,600,560])
+img1 = pyautogui.screenshot('game.png',region=[578,212,748,699])
 print(img1)
+
+img = cv.imread("D:/code/Python/Minesweeper/game.png")
+cv.imshow("Display window", img)
+k = cv.waitKey(0)
