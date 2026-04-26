557ed0d11ad02b15f178ff6fde8299bd554dcf98
Impossible Golem <amriteshsingh132@gmail.com>
2026-04-26 20:45:32 +0530

    cell classification working, revealed vs unrevealed detected(does not work for flags for now)

--- Capture.py
+++ Capture.py
@@ -15,7 +15,23 @@ img = cv.imread("D:/code/Python/Minesweeper/game.png")
 # there is a small error because i cant use decimal val in there it should be 31.34 smt
 for x in range(24):
     for y in range(20):
-        cv.rectangle(img,(x*31,y*31),((x*31)+31,(y*31)+31),(0, 0, 0),2)
+        cv.rectangle(img,(x*31,y*31),((x*31)+31,(y*31)+31),(0, 0, 0),1)
+
+lower = [72, 208, 161]
+upper = [82, 216, 171]
+
+for x in range(24):
+    for y in range(20):
+        cood = img[(y*31)+5][(x*31)+5]
+        if(lower[0] <= cood[0] <= upper[0] and
+        lower[1] <= cood[1] <= upper[1] and
+        lower[2] <= cood[2] <= upper[2]):
+            
+            cv.rectangle(img,(x*31,y*31),((x*31)+31,(y*31)+31),(0, 255, 0),2)
+        else:
+            
+            cv.rectangle(img,(x*31,y*31),((x*31)+31,(y*31)+31),(0, 0, 255),2)
+
 
 cv.imshow("Display window", img)
 k = cv.waitKey(0)
